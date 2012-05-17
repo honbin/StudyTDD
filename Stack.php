0575a40c875fc2ba960b1c47122d16f4525b1e2b
Satoshi Honda <bin.honda@gmail.com>
2012-05-17 20:40:36 +0900

    origin

---
+++ Stack.php
@@ -0,0 +1,39 @@
+<?php
+class Stack
+{
+    private $value = array();
+    private $size  = 0;
+
+    public function isEmpty(){
+        return $this->size === 0;
+    }
+
+    public function push($value) {
+        $this->value[$this->size++] = $value;
+    }
+
+
+    public function top(){
+          $this->emptyCheck();
+          return $this->value[$this->size - 1];
+    }
+
+    public function size(){
+        return $this->size;
+    }
+
+    public function pop(){
+        $this->emptyCheck();
+        --$this->size;
+    }
+
+    private function emptyCheck() {
+        if($this->isEmpty()){
+            throw new EmptyStackException();
+        }
+    }
+}
+
+class EmptyStackException extends Exception
+{
+}
---
+++ TEST/StackTest.php
@@ -0,0 +1,60 @@
+<?php
+require_once 'PHPUnit/Autoload.php';
+require_once '../Stack.php';
+
+class StackTest extends PHPUnit_Framework_TestCase
+{
+    public function setUp(){
+        $this->stack = new Stack();
+    }
+
+    public function testCreate(){
+        $this->assertTrue($this->stack->isEmpty());
+    }
+
+    public function testPushAndTop(){
+        $this->stack->push(1);
+        $this->assertFalse($this->stack->isEmpty());
+        $this->assertEquals(1,  $this->stack->top());
+    }
+
+    public function testPushAndSize(){
+        $this->stack->push(1);
+        $this->assertEquals(1,  $this->stack->size());
+        $this->stack->push(2);
+        $this->assertEquals(2,  $this->stack->size());
+    }
+
+    public function testEmptyPop(){
+        try{
+            $this->stack->pop();
+            $this->fail();
+        } catch(EmptyStackException $e){
+
+        }
+    }
+
+    public function testPushAndPop(){
+        $this->stack->push(1);
+        $this->stack->pop();
+        $this->assertEquals(0,  $this->stack->size());
+    }
+
+    public function testEmptyTop(){
+        try{
+            $this->stack->top();
+            $this->fail();
+        } catch(EmptyStackException $e){
+
+        }
+    }
+
+    public function testPushPushPopTop(){
+        $this->stack->push(1);
+        $this->stack->push(2);
+        $this->assertEquals(2,  $this->stack->size());
+        $this->stack->pop();
+        $this->assertEquals(1,  $this->stack->top());
+    }
+}
+
