328051b0e0f470532423f819165ae8833d99b3df
Satoshi Honda <bin.honda@gmail.com>
2012-05-23 21:17:56 +0900

    5/23日分

---
+++ TEST/TriangleTest.php
@@ -0,0 +1,47 @@
+<?php
+require_once 'PHPUnit/Autoload.php';
+require_once '../Triangle.php';
+
+class TriangleTest extends PHPUnit_Framework_TestCase {
+
+    public function setUp() {
+        $this->tr = new Triangle();
+    }
+
+    public function testValidateInt() {
+        try {
+            $this->tr->validateInt('a', 2, 1);
+        } catch(Exception $e) {
+            $this->assertTrue(true);
+        }
+    }
+
+    public function testValidateRange() {
+        try {
+            $this->tr->validateRange(0, 0, 0);
+        } catch(Exception $e) {
+            $this->assertTrue(true);
+        }
+    }
+
+    public function testValidateFormation() {
+        try {
+            $this->tr->validateFormation(20, 9, 9);
+        } catch(Exception $e) {
+            $this->assertTrue(true);
+        }
+    }
+
+    public function testGetEquilateralId() {
+        $this->assertEquals(1, $this->tr->getGetEquilateralId(9, 9, 9));
+    }
+
+    public function testGetIsoscelesId() {
+        $this->assertEquals(2, $this->tr->getIsoscelesId(10, 9, 9));
+    }
+
+    public function testGetScaleneId() {
+        $this->assertEquals(3, $this->tr->getScaleneId(9, 8, 7));
+    }
+
+}
--- Triangle.php
+++ Triangle.php
@@ -1,14 +1,54 @@
 <?php
 /*
+【演習】 三角形の各辺の長さを表す整数を入力して
 
- 【演習】 三角形の各辺の長さを表す整数を入力して
+・正三角形の場合は1
+・二等辺三角形の場合は2
+・不等辺三角形の場合は3
 
- ・正三角形の場合は1
- ・二等辺三角形の場合は2
- ・不等辺三角形の場合は3
+そして、各辺が三角形を形成しない場合は例外を起こすこと。
 
- そして、各辺が三角形を形成しない場合は例外を起こすこと。
+参考）テスト駆動開発入門 P192の演習問題
 
- 参考）テスト駆動開発入門 P192の演習問題
+*/
 
-* /
+class Triangle {
+
+    public function validateInt($a, $b, $b) {
+        if(!is_int($a) || !is_int($b) || !is_int($c)) {
+            throw new Exception("error int");
+        }
+    }
+
+    public function validateRange($a, $b, $c) {
+        if($a < 1 || $b < 1 || $c < 1) {
+            throw new Exception('error range');
+        }
+    }
+
+    public function validateFormation($a, $b, $c) {
+        $list = array($a, $b, $c);
+        rsort($list);
+        if($list[0] >= ($list[1] + $list[2])) {
+            throw new Exception('error formation');
+        }
+    }
+
+    public function getGetEquilateralId($a, $b, $c) {
+        if($a === $b && $b === $c && $c === $a) {
+            return 1;
+        }
+    }
+
+    public function getIsoscelesId($a, $b, $c) {
+        if($a === $b || $b === $c || $c === $a) {
+            return 2;
+        }
+    }
+
+    public function getScaleneId($a,  $b,  $c) {
+        if($a !== $b && $b !== $c && $c !== $a) {
+            return 3;
+        }
+    }
+}
